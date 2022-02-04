9968a733012003b0e81f9daed84f9e3846652a22
Fernando Perry <fernandoperry1234@gmail.com>
2022-02-04 08:18:35 +0700

    updated compile script

--- make.sh
+++ make.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-python3 -m py_compile ./src/yt-dlp-helper.py
-python3 -m py_compile ./src/yt-dlp-helper-win.py
+python3 -m py_compile ./src/*.py
 mv ./src/__pycache__/yt-dlp-helper.cpython-310.pyc ./yt-dlp-helper.pyc
 mv ./src/__pycache__/yt-dlp-helper-win.cpython-310.pyc ./yt-dlp-helper-win.pyc
 rmdir ./src/__pycache__/
