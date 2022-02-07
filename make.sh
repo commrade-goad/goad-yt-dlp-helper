d77b60bdc426fabccf187012194702045143f52c
Fernando Perry <fernandoperry1234@gmail.com>
2022-02-07 09:55:56 +0700

    edited to add compile v2

--- make.sh
+++ make.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 python3 -m py_compile ./src/*.py
-mv ./src/__pycache__/yt-dlp-helper.cpython-310.pyc ./yt-dlp-helper.pyc
+mv ./src/__pycache__/yt-dlp-helper.cpython-310.pyc ./yt-dlp-helper-v1.pyc
+mv ./src/__pycache__/yt-dlp-helper-v2.cpython-310.pyc ./yt-dlp-helper.pyc
 mv ./src/__pycache__/yt-dlp-helper-win.cpython-310.pyc ./yt-dlp-helper-win.pyc
 rmdir ./src/__pycache__/
 
