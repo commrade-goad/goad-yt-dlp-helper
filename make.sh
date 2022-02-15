666f1bffb764ca7e3ac2f8b5aa468bdeb429913d
Fernando Perry <fernandoperry1234@gmail.com>
2022-02-15 20:33:57 +0700

    added v2-1

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-python3 -m py_compile ./src/*.py
+python3.10 -m py_compile ./src/*.py
 mv ./src/__pycache__/yt-dlp-helper.cpython-310.pyc ./yt-dlp-helper-v1.pyc
 mv ./src/__pycache__/yt-dlp-helper-v2.cpython-310.pyc ./yt-dlp-helper.pyc
 mv ./src/__pycache__/yt-dlp-helper-win.cpython-310.pyc ./yt-dlp-helper-win.pyc
+mv ./src/__pycache__/yt-dlp-helper-v2-1.cpython-310.pyc ./yt-dlp-helper-v2-1.pyc
 rmdir ./src/__pycache__/
-
