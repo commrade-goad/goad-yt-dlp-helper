086b5492dc63b1c2b6c3698ace22c66ebd198e42
Fernando Perry <fernandoperry1234@gmail.com>
2022-02-15 22:14:24 +0700

    changed the rm command.

--- make.sh
+++ make.sh
@@ -3,4 +3,4 @@ python3.10 -m py_compile ./src/*.py
 mv ./src/__pycache__/yt-dlp-helper-v2.cpython-310.pyc ./yt-dlp-helper.pyc
 mv ./src/__pycache__/yt-dlp-helper-win.cpython-310.pyc ./yt-dlp-helper-win.pyc
 mv ./src/__pycache__/yt-dlp-helper-v2-1.cpython-310.pyc ./yt-dlp-helper-v2-1.pyc
-rmdir ./src/__pycache__/
+rm -rf ./src/__pycache__/
