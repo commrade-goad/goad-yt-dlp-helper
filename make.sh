ccca13bbc4f4b53581063460eb012218caa17370
Fernando Perry <fernandoperry1234@gmail.com>
2022-02-21 21:57:26 +0700

    changed the v2.1 to default

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python3.10 -m py_compile ./src/*.py
-mv ./src/__pycache__/yt-dlp-helper-v2.cpython-310.pyc ./yt-dlp-helper.pyc
+mv ./src/__pycache__/yt-dlp-helper-v2.cpython-310.pyc ./yt-dlp-helper-v2.pyc
 mv ./src/__pycache__/yt-dlp-helper-win.cpython-310.pyc ./yt-dlp-helper-win.pyc
-mv ./src/__pycache__/yt-dlp-helper-v2-1.cpython-310.pyc ./yt-dlp-helper-v2-1.pyc
+mv ./src/__pycache__/yt-dlp-helper-v2-1.cpython-310.pyc ./yt-dlp-helper.pyc
 rm -rf ./src/__pycache__/
